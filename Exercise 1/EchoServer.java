2a1f3cf865dc5050ef9763cfc679dae6c7955012
jhbyun <j104onathan@gmail.com>
2017-04-03 16:19:35 -0700

    Update EchoServer.java

--- Exercise 1/EchoServer.java
+++ Exercise 1/EchoServer.java
@@ -1,21 +1,33 @@
 
-import java.io.OutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import javax.net.ServerSocketFactory;
 
 public final class EchoServer {
 
     public static void main(String[] args) throws Exception {
-        try (ServerSocket serverSocket = new ServerSocket(22222)) {
-            while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
+        ServerSocket serverSocket = ServerSocketFactory.getDefault().createServerSocket(22222);
+        System.out.println("Server is Online. You may now run EchoClient.");
+
+        while (true) {
+            try (Socket socket = serverSocket.accept()) {
+            	String address = socket.getInetAddress().getHostAddress();
+            	System.out.printf("Client connected: %s%n", address);
+                //System.out.print("Client connected: " + socket.getInetAddress());
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+                pw.println("Hi, thanks for connecting! Type 'exit' to close.");
+                String input;
+
+                do 
+                {
+                	input = br.readLine();
+                    if (input != null) pw.println("Server> " + input);
                 }
+                while (!input.trim().equals("exit"));
+                System.out.printf("Client disconnected: %s%n", address);  
+                socket.close();
             }
         }
     }
