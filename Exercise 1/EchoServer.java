97ffe0ea47ab299d49bc858583bb5b0426308625
jhbyun <j104onathan@gmail.com>
2017-04-03 17:50:10 -0700

    Update EchoServer.java

--- Exercise 1/EchoServer.java
+++ Exercise 1/EchoServer.java
@@ -5,13 +5,27 @@ import java.net.Socket;
 import javax.net.ServerSocketFactory;
 
 public final class EchoServer {
-
-    public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = ServerSocketFactory.getDefault().createServerSocket(22222);
+	
+	public static void main(String[] args) throws Exception {
+		ServerSocket serverSocket = ServerSocketFactory.getDefault().createServerSocket(22222);
         System.out.println("Server is Online. You may now run EchoClient.");
-
-        while (true) {
-            try (Socket socket = serverSocket.accept()) {
+        try {
+            while (true) {
+                new MultiThreadServer(serverSocket.accept()).start();
+            }
+        } finally {
+            serverSocket.close();
+        }
+    }
+	
+	public static class MultiThreadServer extends Thread {
+        Socket socket;
+        public MultiThreadServer(Socket s) {
+            this.socket = s;
+        }
+        
+        public void run() {        	
+            try {
             	String address = socket.getInetAddress().getHostAddress();
             	System.out.printf("Client connected: %s%n", address);
                 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -27,7 +41,10 @@ public final class EchoServer {
                 while (!input.trim().equals("exit"));
                 System.out.printf("Client disconnected: %s%n", address);  
                 socket.close();
+            } catch (IOException e) {
+            	System.out.println(e);
             }
-        }
-    }
+        }		
+	}	
 }
+
